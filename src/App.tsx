42b394a64f5e330b27e6c5fcda1d11da74f81c29
zhang@heartbeat-med.de <zhang@heartbeat-med.de>
2022-04-16 22:56:15 +0200

    change to hash route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 // import React from 'react';
 import {
-  BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  HashRouter
 } from "react-router-dom";
 
 import routes from './router';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         {routes.map(({ path, Component }) => <Route key={path} path={path} element={<Component />}/>)}
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
